986cf3971c45fe25c77205d04f84b056e0f05c5b
Seah Bei Ying <beiyingseah@gmail.com>
2024-06-18 23:13:52 +0800

    Update copy for profiling page

--- src/app/profiling/page.tsx
+++ src/app/profiling/page.tsx
@@ -18,45 +18,11 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { BiSolidRightArrow } from 'react-icons/bi'
 import { HiLightBulb } from 'react-icons/hi'
-
-export const HOUSEHOLD_TYPE_OPTIONS = [
-  { label: '1-Room', value: 'one_room' },
-  { label: '2-Room', value: 'two_room' },
-  { label: '3-Room', value: 'three_room' },
-  { label: '4-Room', value: 'four_room' },
-  { label: '5-Room', value: 'five_room' },
-  { label: 'Jumbo/Executive', value: 'jumbo' },
-]
-
-export const INSTALLATION_LOCATION_OPTIONS = [
-  { label: 'entire house', value: 'entire_house' },
-  { label: 'bedroom(s)', value: 'bedroom' },
-  { label: 'living room', value: 'living_room' },
-  { label: 'kitchen', value: 'kitchen' },
-]
-
-export const EDUCATIONAL_TIPS = [
-  {
-    question: 'How can I fight climate change from home?',
-    answer:
-      'By selecting energy-efficient appliances, you reduce the demand for electricity from fossil fuels, which cuts down on greenhouse gas emissions.',
-  },
-  {
-    question: 'How can small steps make a big impact?',
-    answer:
-      "Switching to energy-efficient appliances is an easy way to reduce your household's energy consumption, save money, and contribute to a healthier planet.",
-  },
-  {
-    question: 'What does energy efficiency mean?',
-    answer:
-      'Put simply, energy efficiency is all about getting the same performance with less power. Less power consumption means more money saved and a healthier home we call Earth!',
-  },
-  {
-    question: 'Why invest in energy-efficient appliances?',
-    answer:
-      "Energy-efficient appliances don't just help you save money; they're an investment in a sustainable future for future generations.",
-  },
-]
+import {
+  EDUCATIONAL_TIPS,
+  HOUSEHOLD_TYPE_OPTIONS,
+  INSTALLATION_LOCATION_OPTIONS,
+} from '../ui/models/profile-options'
 
 interface FormValues {
   householdType: string
@@ -146,7 +112,7 @@ function FullscreenSpinner({
           textAlign="center"
         >
           {
-            "Then just sit tight, we're crunching the numbers! \n\n Matching your energy profile against air-conditioners out there \n to find the right ones for you..."
+            "Then, sit tight. We're crunching the numbers! \n\n Matching your energy profile against air-conditioners out there \n to find the right ones for you..."
           }
         </Text>
       )}
